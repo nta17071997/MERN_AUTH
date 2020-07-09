fe219bccc38aec26012678d8486a28ceb3c23b16
An <nguyenthanhan.binhminh@gmail.com>
2020-07-09 11:09:20 +0700

    Node Js API

--- server/server.js
+++ server/server.js
@@ -1,15 +1,39 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
-app.get('/api/signup', (req, res) => {
-    res.json({
-        data: "you hit signup endpoint"
-    })
-})
+//connect mongoDB
+mongoose
+  .connect(process.env.DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('DB CONNECTED'))
+  .catch((err) => console.log('DB CONNECTED ERROR', err));
+
+//import routes
+const authRoutes = require('./routes/auth');
+
+//app middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+//app.use(cors());//allows all origin
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
+
+//middleware
+app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`API is running on port ${PORT}`)
-})
+  console.log(`API is running on port ${PORT} - ${process.env.NODE_ENV}`);
+});
